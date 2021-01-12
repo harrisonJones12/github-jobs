ea2b6ba299b3f470d6e3799a8afb2271844bf375
Harrison <harrisonjones5678@gmail.com>
2021-01-11 20:25:12 -0500

    add assets, rename files, and create Header component

---
+++ src/containers/Header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+
+    return (
+        <div className="nav-section-container">
+            <h1 className="nav-heading">devjobs</h1>
+        </div>
+    )
+}
+
+
+export default Header;
