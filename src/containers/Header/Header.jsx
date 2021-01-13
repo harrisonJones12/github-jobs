c16a7c42df5996ca6b3f8dfd000e911f251267f9
Harrison <harrisonjones5678@gmail.com>
2021-01-12 23:14:27 -0500

    add toggle

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,14 +1,18 @@
+import React from "react";
+
+import Toggle from "components/Toggle/Toggle";
+
 const Header = () => {
-
-    return (
-        <div className="nav-section-container">
-            <div className="nav-background">
-            <h1 className="nav-heading">devjobs</h1>
-            </div>
-            
+  return (
+    <div className="nav-section-container">
+      <div className="nav-background">
+        <h1 className="nav-heading">devjobs</h1>
+        <div className="night-day-mode-toggle">
+          <Toggle />
         </div>
-    )
-}
-
+      </div>
+    </div>
+  );
+};
 
 export default Header;
