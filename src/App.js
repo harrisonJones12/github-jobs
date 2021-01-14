eb7dd326c60dedf96fe5e43d26e5f55e90bb7575
Harrison Jones <35752544+harrisonJones12@users.noreply.github.com>
2021-01-13 20:19:24 -0500

    Getting the app up and and adding header component (WIP) (#2)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
-import './App.css';
+import './App.scss';
+
+// TODO add absoloute paths
+
+import Header from "containers/Header/Header";
 
 function App() {
   return (
     <div className="App">
-     
+     <Header />
     </div>
   );
 }
---
+++ src/components/Toggle/Toggle.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Toggle() {
+  return (
+    <>
+      <label class="switch">
+        <input type="checkbox" />
+        <span class="slider round"></span>
+      </label>
+    </>
+  );
+}
