d0b50af9f36b14623904c948831dcf41e7bd94d8
Harrison <harrisonjones5678@gmail.com>
2021-03-11 20:01:24 -0500

    fix broken styles and add breakpoint logic

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,22 @@
+import React, { useEffect, useState } from "react";
+
 const SearchBar = () => {
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+
+  const breakPoint = 1110;
+
+  useEffect(() => {
+    //call function to set viewport width in state
+    const handleWindowResize = () => setViewportWidth(window.innerWidth);
+
+    window.addEventListener("resize", handleWindowResize);
+
+    // cleanup
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
   const filterIcon = (
     <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg">
       <path
@@ -21,14 +39,53 @@ const SearchBar = () => {
     </svg>
   );
 
+  const searchDesktopIcon = (
+    <svg
+      width="24"
+      height="24"
+      xmlns="http://www.w3.org/2000/svg"
+      className="search-desktop-icon"
+    >
+      <path
+        d="M17.112 15.059h-1.088l-.377-.377a8.814 8.814 0 002.15-5.784A8.898 8.898 0 008.898 0 8.898 8.898 0 000 8.898a8.898 8.898 0 008.898 8.899c2.211 0 4.23-.808 5.784-2.143l.377.377v1.081l6.845 6.832 2.04-2.04-6.832-6.845zm-8.214 0A6.16 6.16 0 118.9 2.737a6.16 6.16 0 010 12.322z"
+        fill="#5964e0"
+        fill-rule="nonzero"
+      />
+    </svg>
+  );
+
   const titleSearch = (
-    <div className="search">
-      {searchIcon}
+    <div className="search-icon-input-container">
       <input
         type="text"
         className="title-search-field"
-        placeholder="Filter by title"
+        placeholder={
+          viewportWidth >= breakPoint
+            ? "Filter by title, companies, expertise…"
+            : "Filter by title"
+        }
       />
+    </div>
+  );
+
+  const desktopFilterSection = (
+    <div>
+      <hr className="horizantal-line"></hr>
+      <div className="filter-icon-input-desktop-container">
+        {searchDesktopIcon}
+        <input
+          type="text"
+          className="title-search-field"
+          placeholder="Filter by location…"
+        />
+      </div>
+    </div>
+  );
+
+  const mobileFilterSearch = (
+    <div className="icon-button-group">
+      {filterIcon}
+      <button className="search-button">{searchIcon}</button>
     </div>
   );
 
@@ -37,15 +94,10 @@ const SearchBar = () => {
       <div className="search-container">
         <form className="search-form">
           <fieldset className="field-set">
-            <input
-              type="text"
-              className="title-search-field"
-              placeholder="Filter by title"
-            />
-            <div className="icon-button-group">
-              {filterIcon}
-              <button className="search-button">{searchIcon}</button>
-            </div>
+            {titleSearch}
+            {viewportWidth >= breakPoint
+              ? desktopFilterSection
+              : mobileFilterSearch}
           </fieldset>
         </form>
       </div>
